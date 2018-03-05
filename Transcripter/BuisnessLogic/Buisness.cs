8a8c1602a7e6331c7f6ecdaa568ea09215bb9477
Andrew Swift <559922@hull.ac.uk>
2018-03-05 15:47:42 +0000

    Add project files.

---
+++ Transcripter/BuisnessLogic/Buisness.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Transcripter.BuisnessLogic
+{
+    public class Buisness
+    {
+        private string Fname;
+        private string SecondName;
+
+        public string fname
+        {
+
+            set { Fname = value; }
+            get { return Fname; }
+
+
+        }
+
+        public string secondName
+        { set { SecondName = value; }
+            get { return SecondName; }
+
+        }
+    }
+  }
---
+++ Transcripter/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Transcripter.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Transcripta Search the university records and down load a student transcript.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact";
+
+            return View();
+        }
+    }
+}
